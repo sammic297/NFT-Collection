21f257ecf972ffb456776fde7ddde3eab6aa3421
Odey, Samuel Michael <sammic297@gmail.com>
2022-03-14 16:52:00 +0100

    Create index.js in constants
    Create index.js in constants; linking it to Whitelist-dApp project with Whitelist contract address

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0xf671F58029A7d9657f39Ae04779a3382aC1517B3";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
