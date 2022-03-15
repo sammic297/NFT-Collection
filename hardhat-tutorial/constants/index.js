6cb1c3524a20971776d301f20f22740102533c13
Odey, Samuel Michael <sammic297@gmail.com>
2022-03-15 14:56:13 +0100

    Update Whitelist Contract Address

--- hardhat-tutorial/constants/index.js
+++ hardhat-tutorial/constants/index.js
@@ -1,5 +1,5 @@
 // Address of the Whitelist Contract that you deployed
-const WHITELIST_CONTRACT_ADDRESS = "0xf671F58029A7d9657f39Ae04779a3382aC1517B3";
+const WHITELIST_CONTRACT_ADDRESS = "0x0b59862372690243A193D861F809Cd5e11B19FAD";
 // URL to extract Metadata for a Crypto Dev NFT
 const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
 
